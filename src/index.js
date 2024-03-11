92c028a2998cafdc50836207fe6419ee351897ec
binh.phan@smsdome.com <vominhtrungpro@gmail.com>
2024-03-11 16:31:27 +0700

    update Unauthorized

--- src/index.js
+++ src/index.js
@@ -4,15 +4,38 @@ import './index.css';
 import App from './App';
 import WebSocketComponent from './WebSocket';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import Main from './pages/Main'
+import Request from './pages/Request'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/main",
+    element: <Main />,
+  },
+  {
+    path: "/request",
+    element: <Request />,
+  },
+]);
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId="326313607776-macv5cle3gs1ehtb53rcm95je9rd3tlh.apps.googleusercontent.com">
-      <App />
+      <RouterProvider router={router} />
     </GoogleOAuthProvider>;
   </React.StrictMode>
 );
---
+++ src/pages/Request.js
@@ -0,0 +1,185 @@
+import React from 'react';
+import YouTube from 'react-youtube';
+import { GoogleLogin } from '@react-oauth/google';
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+function getVideoIdFromUrl(url) {
+  // Kiểm tra xem url có phải là một chuỗi không
+  if (typeof url !== 'string') {
+    console.error('URL must be a string');
+    return null;
+  }
+
+  const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+  const match = url.match(regExp);
+  if (match && match[7].length === 11) {
+    return match[7];
+  } else {
+    console.error('Invalid YouTube URL');
+    return null;
+  }
+}
+class Request extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videoIndex: 0,
+      videoUrls: [],
+      player: null
+    };
+  }
+
+  handleInputChange(event) {
+    this.setState({ inputValue: event.target.value });
+  }
+
+  logInputValue() {
+    const apiUrl = 'http://127.0.0.1:5000/api/insert_music';
+    const data = { url: this.state.inputValue };
+
+    axios.post(apiUrl, data, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization:localStorage.getItem('token')
+      }
+    }
+    ).then(response => {
+      console.log(response.data.Message)
+      toast(response.data.Message)
+    })
+    .catch(error => {
+        if (error.response && error.response.status === 401) {
+          toast('Unauthorized')
+        } else {
+          toast('An error occurred. Please try again later.');
+        }
+      });
+  }
+
+
+//   playVideoByUrl(url) {
+//     const videoId = getVideoIdFromUrl(url);
+//     if (this.state.player) {
+//       this.state.player.loadVideoById(videoId);
+//     }
+//   }
+
+
+//   componentDidMount() {
+//     this.socket = new WebSocket('ws://127.0.0.1:8765');
+
+//     this.socket.onopen = () => {
+//       console.log('WebSocket connected');
+//     };
+
+//     this.socket.onmessage = (event) => {
+//       console.log('Received message:', event.data);
+//       const newVideoId = event.data;
+//       this.setState((prevState) => ({
+//         videoUrls: [...prevState.videoUrls, newVideoId],
+//       }), () => {
+//         if (this.state.videoUrls.length === 1 && this.state.player) {
+//           this.state.player.playVideo();
+//         }
+//       });
+//     };
+
+//     this.socket.onerror = (error) => {
+//       console.error('WebSocket error:', error);
+//     };
+
+//     this.socket.onclose = () => {
+//       console.log('WebSocket disconnected');
+//     };
+//   }
+
+//   componentWillUnmount() {
+//     this.socket.close();
+//   }
+
+//   connectWebSocket() {
+//     const ws = new WebSocket('ws://127.0.0.1:8765');
+
+//     ws.onopen = () => {
+//       console.log('WebSocket connected');
+//     };
+    
+//     ws.onmessage = (event) => {
+//       console.log('Received message:', event.data);
+//       const newVideoId = event.data;
+//       if (this.state.isMounted) {
+//         this.setState((prevState) => ({
+//           videoUrls: [...prevState.videoUrls, newVideoId],
+//         }), () => {
+//           if (this.state.videoUrls.length === 1 && this.state.player) {
+//             this.state.player.playVideo();
+//           }
+//         });
+//       }
+//     };
+
+    
+//     ws.onerror = (error) => {
+//       console.error('WebSocket error:', error);
+//       setTimeout(() => {
+//         this.connectWebSocket();
+//       }, 2000);
+//     };
+    
+//     ws.onclose = () => {
+//       console.log('WebSocket closed');
+//       setTimeout(() => {
+//         this.connectWebSocket();
+//       }, 2000);
+//     };
+//   }
+
+//   _onReady(event) {
+//     this.setState({
+//       player: event.target,
+//     });
+//     event.target.playVideo();
+//   }
+
+//   _onEnd(event) {
+//     this.setState((prevState) => ({
+//       videoIndex: (prevState.videoIndex + 1) % prevState.videoUrls.length,
+//     }), () => {
+//       if (this.state.player) {
+//         this.state.player.loadVideoById(getVideoIdFromUrl(this.state.videoUrls[this.state.videoIndex]));
+//       }
+//     });
+//   }
+
+  render() {
+    const { videoUrls, videoIndex } = this.state;
+    const opts = {
+      height: '390',
+      width: '640',
+      playerVars: {
+        autoplay: 1,
+      },
+    };
+
+    return (
+      <div>
+        <input type="text" value={this.state.inputValue} onChange={this.handleInputChange.bind(this)} />
+        <button onClick={this.logInputValue.bind(this)}>Log Input Value</button>
+        <ToastContainer />
+        <GoogleLogin
+          onSuccess={credentialResponse => {
+            localStorage.setItem('token', credentialResponse.credential);
+          }}
+          onError={() => {
+            console.log('Login Failed');
+          }}
+        />
+      </div>
+    )
+  }
+}
+
+export default Request;
